6e98704bcd88f385c286c247f8d47ba4e2e47c83
Breno Fortunato <BrenoSilvaFortunato@gmail.com>
2022-06-23 21:43:15 -0300

    add tests

---
+++ Pipeline-GithubActions.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Xunit;
+
+namespace Pipeline_GithubActions.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.True(true);
+        }
+
+    }
+}
